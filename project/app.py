6848b6ac71039d95e3771c9f683e5194f704c009
HC im <haechan3@naver.com>
2020-07-24 17:40:21 +0900

    2020-07-24

--- project/app.py
+++ project/app.py
@@ -3,33 +3,37 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('01_home.html')
+    menu = {'home':True, 'rgrs':False, 'stmt':False, 'clsf':False, 'clst':False, 'user':False}
+    return render_template('home.html', menu=menu)
 
-@app.route('/typo')
-def typo():
-    return render_template('03_typography.html')
-
-@app.route('/iris', methods=['GET', 'POST'])
-def iris():
+@app.route('/regression', methods=['GET', 'POST'])
+def regression():
+    menu = {'home':False, 'rgrs':True, 'stmt':False, 'clsf':False, 'clst':False, 'user':False}
     if request.method == 'GET':
-        return render_template('12_form-iris.html')
+        return render_template('regression.html', menu=menu)
     else:
-        slen1 = float(request.form['slen']) * 2
-        plen1 = float(request.form['plen']) * 2
-        pwid1 = float(request.form['pwid']) * 2
-        species1 = int(request.form['species'])
-        comment1 = request.form['comment']
-        return render_template('12_iris-result.html', 
-                slen=slen1, plen=plen1, pwid=pwid1, species=species1, comment=comment1)
+        sp_names = ['Setosa', 'Versicolor', 'Virginica']
+        slen = float(request.form['slen'])      # Sepal Length
+        plen = float(request.form['plen'])      # Petal Length
+        pwid = float(request.form['pwid'])      # Petal Width
+        sp = int(request.form['species'])       # Species
+        species = sp_names[sp]
+        swid = 0.63711424 * slen - 0.53485016 * plen + 0.55807355 * pwid - 0.12647156 * sp + 0.78264901
+        swid = round(swid, 4)
+        iris = {'slen':slen, 'swid':swid, 'plen':plen, 'pwid':pwid, 'species':species}
+        return render_template('reg_result.html', menu=menu, iris=iris)
 
-@app.route('/project')
-def project():
-    return render_template('17_templates.html')
+@app.route('/sentiment')
+def sentiment():
+    pass
 
-@app.route('/hello')
-@app.route('/hello/<name>')
-def hello(name=None):
-    return render_template('hello.html', name=name)
+@app.route('/classification')
+def classification():
+    pass
+
+@app.route('/clustering')
+def clustering():
+    pass
 
 if __name__ == '__main__':
     app.run(debug=True)
